f3b956cfcde4b3d60a25f803ffa963951e1ac454
Breno Augusto <brenoaugusto98@gmail.com>
2022-05-07 20:41:43 -0300

    deploy

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "5598fddcaf4617",
+    pass: "3b82ca11acb154"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({subject, body}: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Anna Costa <annacosta.raquel@gmail.com>',
+      subject,
+      html: body
+    })
+  };  
+}
